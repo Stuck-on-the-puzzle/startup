4e9443412b25939f5f1644beed16cdd9a0b05749
Stuck-on-the-puzzle <nathanjstucki@gmail.com>
2025-02-12 20:29:42 -0700

    Reorganized files to comply with react better. Enabled react and created app component

---
+++ src/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+// import Button from 'react-bootstrap/Button';
+
+// export function NavButton({ text, url }) {
+//   const navigate = useNavigate();
+//   return (
+//     <Button variant="primary" onClick={() => navigate({ url })}>
+//       {text}
+//     </Button>
+//   );
+// }
+
+export default function App() {
+    return <div className="body bg-dark text-light">App will display here</div>;
+}
