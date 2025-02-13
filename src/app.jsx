836e6dee8c48201f69be0fc9c8c44f687e35f1e1
Stuck-on-the-puzzle <nathanjstucki@gmail.com>
2025-02-12 20:49:51 -0700

    Fixed some wierd formatting when creating app.jsx created bade app.jsx file but have not configured the changing main portion

--- src/app.jsx
+++ src/app.jsx
@@ -13,5 +13,23 @@ import './app.css';
 // }
 
 export default function App() {
-    return <div className="body bg-dark text-light">App will display here</div>;
+    return <div className="body bg-dark text-light"><header>
+    <h1>BookClub!📚</h1>
+    <nav>
+      <ul>
+        <li><a href="index.html" className="makelinkbig">Login</a></li>
+        <li><a href="home.html" className="makelinkbig">Home</a></li>
+        <li><a href="readbook.html" className="makelinkbig">BooksRead</a></li>
+        <li><a href="notreadbook.html" className="makelinkbig">BooksNotRead</a></li>
+      </ul>
+    </nav>
+    <hr />
+  </header>
+
+  <main className="container-fluid">App components go here</main>
+
+  <footer>
+      <div>Author: Nathan Stucki</div>
+      <a href="https://github.com/Stuck-on-the-puzzle/startup.git" className="makelinkbig">GitHub</a>
+  </footer></div>;
 }
